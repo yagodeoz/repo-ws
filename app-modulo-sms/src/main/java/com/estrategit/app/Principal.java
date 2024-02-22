6d1d8a63ec3a24ebf1ed5428f7fabc644227d385
Byron <byronsantiago@gmail.com>
2024-02-22 12:29:36 -0500

    Se agregan las clases al proyecto

---
+++ app-modulo-sms/src/main/java/com/estrategit/app/Principal.java
@@ -0,0 +1,10 @@
+package com.estrategit.app;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hola mundo");
+	}
+
+}
